04294b4e058eb4a5fff8ca1a986dbe6ce75a939f
Nathan Wells <sungkhum@gmail.com>
2025-03-20 10:56:11 -0700

    Update

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,35 +1,30 @@
 module.exports = [
   'strapi::logger',
   "strapi::errors",
-  /* Replace 'strapi::security', with this snippet */
-      /* Beginning of snippet */
-      {
-        name: 'strapi::security',
-        config: {
-          contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-              'connect-src': ["'self'", 'https:'],
-              'img-src': [
-                "'self'",
-                'data:',
-                'blob:',
-                'dl.airtable.com',
-                'yourBucketName.s3.yourRegion.amazonaws.com',
-              ],
-              'media-src': [
-                "'self'",
-                'data:',
-                'blob:',
-                'dl.airtable.com',
-                'yourBucketName.s3.yourRegion.amazonaws.com',
-              ],
-              upgradeInsecureRequests: null,
-            },
-          },
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${env("AWS_BUCKET_NAME")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${env("AWS_BUCKET_NAME")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
         },
       },
-      /* End of snippet */
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -6,15 +6,18 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          ACL: env('AWS_ACL', 'public-read'),
-          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-          Bucket: env('AWS_BUCKET'),
+        s3Options: {
+          region: env("AWS_REGION"),
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          params: {
+            ACL: "public-read",
+            Bucket: env("AWS_BUCKET_NAME"),
+          },
         },
       },
       actionOptions: {
